510218defa808e22670960265a11c157e32d2ce4
Waweshirush <99341580+Waweshirush@users.noreply.github.com>
2022-06-12 00:37:03 +0300

    task two

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        print("{:c}".format(ch), end='')
+string=raw_input("Enter string:")
+count1=0
+count2=0
+for i in string:
+      if(i.islower()):
+            count1=count1+1
+      elif(i.isupper()):
+            count2=count2+1
+print("The number of lowercase characters is:")
+print(count1)
+print("The number of uppercase characters is:")
+print(count2)
