f7a73eef2ea2c0236e55f05fdfbd0f6027ad8d79
Waweshirush <99341580+Waweshirush@users.noreply.github.com>
2022-06-12 01:04:27 +0300

    task fourteen

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for ch in reversed(range(97, 123)):
-    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
+num = 0
+for x in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(x - num)), end="")
+    num = 32 if num == 0 else 0
