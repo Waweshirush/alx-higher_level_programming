dbc45322f07c85347ceca803b6cf6e1a30909da0
Waweshirush <99341580+Waweshirush@users.noreply.github.com>
2022-07-07 00:23:25 +0300

    task one

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,10 +6,13 @@
 
 def number_of_lines(filename=""):
     """ Function that reads from a file and prints its number of lines
+    
     Args:
         filename: filename
+        
     Raises
         Exception: when the file can be opened
+        
     """
     n_lines = 0
     with open(filename, 'r', encoding="utf-8") as f:
